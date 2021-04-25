865d64ef6aa4cbe379828fe3eaf3f0b105c764b0
Raheem <67774375+raheemadamboev@users.noreply.github.com>
2021-04-25 16:35:28 +0500

    item restaurant
    Took 7 minutes

--- app/src/main/java/xyz/teamgravity/offlinecaching/activity/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/offlinecaching/activity/MainActivity.kt
@@ -3,10 +3,15 @@ package xyz.teamgravity.offlinecaching.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import xyz.teamgravity.offlinecaching.R
+import xyz.teamgravity.offlinecaching.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
