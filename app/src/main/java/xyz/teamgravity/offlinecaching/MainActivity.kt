0e90dacc001ba7b67b4a1d5233f5dae442f19363
Raheem <67774375+raheemadamboev@users.noreply.github.com>
2021-04-25 16:05:58 +0500

    initial

---
+++ app/src/main/java/xyz/teamgravity/offlinecaching/MainActivity.kt
@@ -0,0 +1,11 @@
+package xyz.teamgravity.offlinecaching
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
